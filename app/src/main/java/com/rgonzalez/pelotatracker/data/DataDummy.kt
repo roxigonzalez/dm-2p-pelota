1453cef9277dbfef05a1f882dbca68d4f8d70334
Roxy <roxigo@hotmail.com>
2023-05-23 09:08:51 -0600

    Model added in the last commit - here was added the data dummy

--- app/src/main/java/com/rgonzalez/pelotatracker/data/DataDummy.kt
+++ app/src/main/java/com/rgonzalez/pelotatracker/data/DataDummy.kt
@@ -2,3 +2,9 @@ package com.rgonzalez.pelotatracker.data
 
 import com.rgonzalez.pelotatracker.data.model.PelotaModel
 
+var balls = mutableListOf<PelotaModel>(
+    PelotaModel("soccer", "white"),
+    PelotaModel("basketball", "black"),
+    PelotaModel("volleyball", "white"),
+    PelotaModel("", "white")
+)
