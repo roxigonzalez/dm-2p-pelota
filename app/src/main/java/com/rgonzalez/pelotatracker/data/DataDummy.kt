4455921d31bad7526c92f98664243ffac9b78283
Roxy <roxigo@hotmail.com>
2023-05-23 09:07:14 -0600

    first commit :tada:

---
+++ app/src/main/java/com/rgonzalez/pelotatracker/data/DataDummy.kt
@@ -0,0 +1,4 @@
+package com.rgonzalez.pelotatracker.data
+
+import com.rgonzalez.pelotatracker.data.model.PelotaModel
+
---
+++ app/src/main/java/com/rgonzalez/pelotatracker/data/model/PelotaModel.kt
@@ -0,0 +1,6 @@
+package com.rgonzalez.pelotatracker.data.model
+
+data class PelotaModel(
+    val type: String,
+    val color: String
+)
