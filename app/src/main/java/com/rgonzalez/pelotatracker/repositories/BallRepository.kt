8e69bb75c8e633088761be8336b0e3d891af9867
Roxy <roxigo@hotmail.com>
2023-05-23 09:15:47 -0600

    Added repository ballRepository

--- app/src/main/java/com/rgonzalez/pelotatracker/repositories/BallRepository.kt
+++ app/src/main/java/com/rgonzalez/pelotatracker/repositories/BallRepository.kt
@@ -1,4 +1,11 @@
 package com.rgonzalez.pelotatracker.repositories
 
-class PelotaRepository {
+import com.rgonzalez.pelotatracker.data.model.BallModel
+
+class BallRepository(private val balls:MutableList<BallModel>){
+    fun getBalls() = balls
+
+    fun addBall(ball: BallModel) {
+        balls.add(0, ball)
+    }
 }
